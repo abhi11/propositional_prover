90a712106def2a3ab720e2733c08347b0540185a
Abhishek <abhishek.bhattacharjee11@gmail.com>
2018-11-05 15:36:55 -0500

    Fix PEP-8 issues

--- prover.py
+++ prover.py
@@ -15,6 +15,7 @@ INVALID = 'E'
 TAUTOLOGY = 'T'
 UNSATISFIABLE = 'U'
 
+
 def NOT_Expr(p):
     return '(not {0})'.format(p)
 
@@ -32,19 +33,20 @@ def AND_Expr(p, q):
 
 
 def make_saner(s):
-    '''
+    """
     Takes a string and removes unecessary spaces newline etc
-    '''
+    """
     tokens = s.split()
     saner = ' '.join(tokens)
     return saner
 
+
 def check_basic_sanity(F):
-    '''
+    """
     Number of OPENING and CLOSING  brackets should be equal
     Returns False if not
     Also checks if there's any special character, if present returns False
-    '''
+    """
     op = 0
     cl = 0
     for c in F:
@@ -63,16 +65,17 @@ def check_basic_sanity(F):
 
     return (cl == op)
 
-def isPure(p):
-    '''
+
+def is_pure(p):
+    """
     Checks if the operand is pure or it is an indirection
-    '''
+    """
     if 'OPERANDS' in p:
         return False
     return True
 
-## it is important to use `OPERANDS` here
-def handle_operation(op, stack_operands):
+
+def handle_operation(op, stack_operands):  # it is important to use `OPERANDS` here
     if op not in VALID_OPS:
         return INVALID
 
@@ -89,7 +92,7 @@ def handle_operation(op, stack_operands):
             return INVALID
 
         var = ''
-        if isPure(p):
+        if is_pure(p):
             var = 'OPERANDS["{0}"]'.format(p)
         else:
             var = p
@@ -97,7 +100,7 @@ def handle_operation(op, stack_operands):
 
     var1 = ''
     p = stack_operands.pop()
-    if isPure(p):
+    if is_pure(p):
         var1 = 'OPERANDS["{0}"]'.format(p)
     else:
         var1 = p
@@ -107,7 +110,7 @@ def handle_operation(op, stack_operands):
 
     var2= ''
     q = stack_operands.pop()
-    if isPure(q):
+    if is_pure(q):
         var2 = 'OPERANDS["{0}"]'.format(q)
     else:
         var2 = q
@@ -126,15 +129,15 @@ def handle_operation(op, stack_operands):
     if op == AND:
         return AND_Expr(var1, var2)
 
+
 def get_formula(F):
     stack_operands = []
 
     # dict to store operand names
-    ## The name is important here it is used by the eval() mehtod
+    # The name is important here it is used by the eval() method
     operands = {}
 
     revF = reversed(F)
-    # work on a list
     rev_characters = list(revF)
 
     buf = ''
@@ -162,7 +165,7 @@ def get_formula(F):
             buf = c + buf
             continue
 
-        ## Operation here
+        # Operation here
         if (c == OPENING_BRACKET) or ((c == SPACE) and (buf in VALID_OPS)):
             # if empty stack for braces then ill formed expression
             if len(stack_operands) < 1:
@@ -196,7 +199,7 @@ def get_formula(F):
             # invalid character unsupported
             return INVALID, operands
 
-    ## buf should be empty for a valid formula
+    # buf should be empty for a valid formula
     if buf != "":
         return INVALID, operands
 
@@ -208,11 +211,11 @@ def get_formula(F):
 
 
 def permutations_for_truth_values(n):
-    '''
+    """
     Takes n as the number of bits and
     returns all possible permuations of bits
     as a matrix(list of list)
-    '''
+    """
     bools = [True, False]
     perms = itertools.product(bools, repeat=n)
     perms = list(perms)
@@ -223,7 +226,7 @@ def permutations_for_truth_values(n):
 def prove_formula(F):
     F = make_saner(F)
 
-    ## Special case when just one literal
+    # Special case when just one literal
     tokens = F.split()
     if len(tokens) == 1 and tokens[0].isalnum() and tokens[0].islower() and tokens[0] not in VALID_OPS:
         return 1
@@ -238,14 +241,14 @@ def prove_formula(F):
     number_of_operands = len(OPERANDS)
     permutations = permutations_for_truth_values(number_of_operands)
 
-    ## Get all keys first as a list
+    # Get all keys first as a list
     keys = list(OPERANDS.keys())
 
-    ## Get Truth value for all combinations
+    # Get Truth value for all combinations
     results = []
     for permutation in permutations:
         pointer = 0
-        ## Put values in OPERANDS
+        # Put values in OPERANDS
         while pointer < number_of_operands:
             OPERANDS[keys[pointer]] = permutation[pointer]
             pointer = pointer + 1
